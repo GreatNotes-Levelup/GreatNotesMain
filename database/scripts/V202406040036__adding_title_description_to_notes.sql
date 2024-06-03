fdd1d318ca896cb960d61a752c1594de0e8f9b16
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2024-06-04 01:08:03 +0200

    Droping an re-adding constraint.

--- database/scripts/V202406040036__adding_title_description_to_notes.sql
+++ database/scripts/V202406040036__adding_title_description_to_notes.sql
@@ -9,6 +9,7 @@ CREATE TABLE "Notes_new" (
     "is_public" BOOLEAN NOT NULL DEFAULT '0'
 );
 
+
 INSERT INTO "Notes_new" (
     "note_id",
     "title",
@@ -21,19 +22,22 @@ INSERT INTO "Notes_new" (
 )
 SELECT
     "note_id",
-    '',
+    '', 
     '',
     "owner_id",
-    "content_url",
+    "content_url", 
     "created_at",
     "updated_at",
     "is_public"
 FROM
     "Notes";
 
+
 ALTER TABLE "NoteAccess" DROP CONSTRAINT "noteaccess_note_id_foreign";
 
+
 DROP TABLE "Notes";
+
 
 ALTER TABLE "Notes_new" RENAME TO "Notes";
 
@@ -41,6 +45,7 @@ ALTER TABLE "Notes" ADD PRIMARY KEY("note_id");
 
 COMMENT ON COLUMN "Notes"."is_public" IS 'Private by default';
 
+ALTER TABLE "NoteAccess" DROP CONSTRAINT "noteaccess_user_id_foreign"; -- Drop the existing constraint
 ALTER TABLE "NoteAccess" ADD CONSTRAINT "noteaccess_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "Users"("user_id");
 ALTER TABLE "Notes" ADD CONSTRAINT "notes_owner_id_foreign" FOREIGN KEY("owner_id") REFERENCES "Users"("user_id");
 ALTER TABLE "NoteAccess" ADD CONSTRAINT "noteaccess_note_id_foreign" FOREIGN KEY("note_id") REFERENCES "Notes"("note_id");
