b951d3b9f688e911bab441ddb1546d4ad39e1955
Sean van Wyk <sean.vanwyk@bbd.co.za>
2024-06-05 23:14:44 +0200

    Implement rate limiting

--- server/index.js
+++ server/index.js
@@ -4,11 +4,27 @@ import auth from './routes/auth.js';
 import users from './routes/users.js';
 import cors from 'cors';
 import path from 'path';
+import authMiddleware from './middleware/authMiddleware.js';
+import { rateLimit } from 'express-rate-limit'
 
 import { configDotenv } from 'dotenv';
 
 configDotenv();
 const app = express();
+const tokenLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 500, 
+	standardHeaders: 'draft-7', 
+	legacyHeaders: false, 
+  keyGenerator: (req, res) => res.locals.user.username,
+})
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 50, 
+	standardHeaders: 'draft-7', 
+	legacyHeaders: false,
+})
 
 app.use(express.json());
 //Print node env
@@ -22,8 +38,14 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'] 
 }));
 
-app.use('/api/notes', notes);
+// Endpoints for authorizing user - Limits on IP address
+app.use('/api/auth', authLimiter);
 app.use('/api/auth', auth);
+
+// Main API Endpoints - Limits on username for user
+app.use('/api', authMiddleware);
+app.use('/api', tokenLimiter);  
+app.use('/api/notes', notes);
 
 app.use(express.static('dist'));
 const __dirname = path.resolve(path.dirname(''));
